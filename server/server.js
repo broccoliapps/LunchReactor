c9b0472e557bd7ccaf0e2a40ccbc620c0d9bd069
dannydelott <dannydelott@gmail.com>
2015-06-07 23:29:44 -0700

    Add basic static file express server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// Default modules
+var path = require('path'); // https://nodejs.org/api/path.html#path_path
+
+// Imports NPM modules
+var express = require('express');
+
+// Set environment variables
+var port = process.env.port || 3000;
+
+// Creates the instance of express that will become the server
+var app = express();
+
+// Configure express.static middleware to serve static assets
+// http://expressjs.com/starter/static-files.html
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Start the server
+var server = app.listen(port, function() {
+  console.log('Listening on port:', port);
+});
