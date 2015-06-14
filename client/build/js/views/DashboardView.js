1d0eddc7c69519344db4ae4a97976056602a4daf
dannydelott <dannydelott@gmail.com>
2015-06-13 17:04:19 -0700

    Add model and events to RsvpView

--- client/build/js/views/DashboardView.js
+++ client/build/js/views/DashboardView.js
@@ -3,7 +3,9 @@ var DashboardView = Backbone.View.extend({
   id: 'dashboard',
 
   initialize: function() {
-    this.rsvp = new RsvpView();
+    this.rsvp = new RsvpView({
+      model: new RSVP()
+    });
     this.counter = new CounterView();
     this.render();
   },
--- client/build/js/views/RsvpView.js
+++ client/build/js/views/RsvpView.js
@@ -1,15 +1,48 @@
 var RsvpView = Backbone.View.extend({
 
-  id: 'rsvp',
+  events: {
+    'click': 'doRSVP',
+    'mouseenter': 'toggleFrost',
+    'mouseleave': 'toggleFrost'
+  },
 
-  className: 'circle',
+  template: _.template(
+    '<div id="rsvp" class="circle">' +
+    '<p><span class="rsvp">RSVP</span>' +
+    '<%= timeLeft %></p></div>'
+  ),
+  frost: '<div id="frost" class="circle hide"></div>',
+  success: '<div id="success" class="circle"><p>✓</p></div>',
 
   initialize: function() {
 
-    this.render();
+    var hasRSVPed = this.model.get('hasRSVPed');
+
+    // Has user RSVPed before?
+    var html = hasRSVPed ? this.success : this.template({
+      timeLeft: this.model.get('timeLeft')
+    });
+
+    // Is it past expiration?
+    if (!hasRSVPed && !this.model.get('isExpired')) {
+      html += this.frost;
+    }
+
+    this.render(html);
   },
 
-  render: function() {
+  render: function(html) {
+    this.$el.html(html);
+  },
 
+  toggleFrost: function() {
+    this.$el.children('#frost').toggleClass('hide');
+  },
+
+  doRSVP: function() {
+
+    // TODO
+    // Validate time, check if past expiration
+    // Perform RSVP
   }
 });
