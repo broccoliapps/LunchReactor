0482e324aad296467fb560766b1b281068abdbf3
dannydelott <dannydelott@gmail.com>
2015-06-15 19:35:51 -0700

    Add user events to MessageView

--- client/build/js/views/MessageView.js
+++ client/build/js/views/MessageView.js
@@ -2,18 +2,28 @@ var MessageView = Backbone.View.extend({
 
   id: 'message',
 
+  events: {
+    'click #link-auth': 'showAuthDialog',
+    'click #link-auth2': 'showAuthDialog'
+  },
+
   template: _.template(
     '<p>' +
-    '<a id="link-auth" rel="leanModal" href="#modal-auth">Sign in</a> ' +
-    'or <a id="link-auth2" rel="leanModal" href="#modal-auth">create an account</a> in seconds.' +
+    '<a id="link-auth">Sign in</a> ' +
+    'or <a id="link-auth2" onclick=>create an account</a> in seconds.' +
     '</p>'),
 
+
   initialize: function() {
+    this.auth = new AuthDialogView();
     this.render();
   },
 
   render: function() {
     this.$el.html(this.template());
-  }
+  },
 
+  showAuthDialog: function() {
+    console.log('showAuthDialog');
+  }
 });
